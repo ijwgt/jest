e5cdfb5ca216000374d552f2cd238e11637ec484
ijwgt <66661966@bk.ru>
2024-08-24 22:27:18 +0300

    fix

--- src/calculate.js
+++
@@ -1,5 +0,0 @@
-export function calculateTotal(purchases) {
-	return purchases.reduce((total, item) => {
-		return total + (item.count * item.price);
-	}, 0);
-}
--- src/test/test.js
+++ src/test/test.js
@@ -1,31 +1,4 @@
-import {calculateTotal} from '../calculate';
 import sortPlayers from '../game';
-
-test('basic test', () => {
-	const result = 4;
-	expect(result).toBe(4);
-});
-
-test('calculateTotal sum', () => {
-	const list = [
-		{
-			id: 456,
-			name: 'Война и мир',
-			count: 3,
-			price: 400
-		},
-		{
-			id: 77,
-			name: 'JavaScript',
-			count: 1,
-			price: 1300
-		}
-	]
-	const result = calculateTotal(list);
-	expect(result).toBe(2500);
-})
-
-
 
 test('checking player sorting', () => {
   const players = [
